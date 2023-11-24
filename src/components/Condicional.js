7b1548eed2775d07325455695dea5c428253babe
Marcos Santos <marcos.infosantos@gmail.com>
2023-11-23 22:29:18 -0300

    23-11-2023
    Renderização condicional

--- src/components/Condicional.js
+++ src/components/Condicional.js
@@ -1,15 +1,32 @@
+import { useState } from "react";
+
 function Condicional() {
+
+    const [email, setEmail] = useState()
+    const [userEmail, setUserEmail] = useState()
 
     function Teste(e) {
         e.preventDefault()
-        console.log("teste");
+        setUserEmail(email)
     }
+
+    function limparEmail(e) {
+        e.preventDefault()
+        setUserEmail('')
+    }
+
     return (
         <div>
             <h2>Cadastre seu email</h2>
             <form type="submit" action="">
-                <input type="email" name="" id="" placeholder="Digite seu email..." />
+                <input type="email" name="" id="" placeholder="Digite seu email..." onChange={(e) => setEmail(e.target.value)}/>
                 <button onClick={Teste} type="submit">Enviar-email</button>
+                {userEmail &&(
+                    <div>
+                        <p>O email do usuario é: {userEmail}</p>
+                        <button onClick={limparEmail}>Limpe email</button>
+                    </div>
+                )}
             </form>
         </div>
     )
